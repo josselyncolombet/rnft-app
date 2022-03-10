8e18f95992da2f40683c5881f6ec1b0464929376
Josselyn COLOMBET <j.colombet@ecole-isitech.fr>
2022-03-10 04:40:36 +0100

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,129 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
+import * as ImageManipulator from 'expo-image-manipulator';
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [cameraRef, setCameraRef] = useState(null)
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const { height, width } = Dimensions.get("window");
+  const maskRowHeight = Math.round((height - 200) / 20);
+  const maskColWidth = (width - 200) / 2;
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Camera style={styles.camera} type={type} ref={ref => {setCameraRef(ref)}}>
+      <View style={styles.maskOutter}>
+              <View
+                style={[
+                  { flex: maskRowHeight },
+                  styles.maskRow,
+                  styles.maskFrame
+                ]}
+              />
+
+              <View style={[{ flex: 40 }, styles.maskCenter]}>
+                <View style={[{ width: maskColWidth }, styles.maskFrame]} />
+                <View style={styles.maskInner}>
+                  <View
+                    style={{
+                      position: "absolute",
+                      top: 0,
+                      left: 0,
+                      width: "100%",
+                      height: 10,
+                      borderColor: "#FFFFFF",
+                      borderTopWidth: 1,
+                    }}
+                  />
+                  <View
+                    style={{
+                      position: "absolute",
+                      bottom: 0,
+                      left: 0,
+                      width: "100%",
+                      height: 10,
+                      borderColor: "#FFFFFF",
+                      borderBottomWidth: 1,
+                    }}
+                  />
+                  <View
+                    style={{
+                      position: "absolute",
+                      top: 0,
+                      left: 0,
+                      width: 20,
+                      height: "100%",
+                      borderColor: "#FFFFFF",
+                      borderLeftWidth: 1,
+                    }}
+                  />
+                  <View
+                    style={{
+                      position: "absolute",
+                      top: 0,
+                      right: 0,
+                      width: 20,
+                      height: "100%",
+                      borderColor: "#FFFFFF",
+                      borderRightWidth: 1,
+                    }}
+                  />
+                  
+                </View>
+                <View style={[{ width: maskColWidth }, styles.maskFrame]} />
+              </View>
+              <View
+                style={[
+                  { flex: maskRowHeight },
+                  styles.maskRow,
+                  styles.maskFrame
+                ]}
+              >          
+              <TouchableOpacity style={{alignSelf: 'center', marginBottom: 20}} onPress={async() => {
+            if(cameraRef) {
+              let photo = await cameraRef.takePictureAsync({ skipProcessing: true });
+              const manipResult = await ImageManipulator.manipulateAsync(
+                photo.uri,
+                [{ crop: {height: 1500, originX: 250, originY: 1300, width: 1500} }],
+                { compress: 1, format: ImageManipulator.SaveFormat.PNG });
+              
+              
+              const formData = new FormData()
+              formData.append('picture', { uri: manipResult.uri, name: 'picture.jpg', type: 'image/jpg' })
+              const request = await fetch('https://rnft-upload-api.herokuapp.com/api/upload', {
+                method:'POST',
+                body: formData
+              })
+
+              const result = await request.json()
+              console.log(result)
+            }
+          }}>
+            <View style={{ backgroundColor:"white", marginTop:50, padding: 15}}>
+              <Text style={{fontSize: 12, color: "black"}}> Take Picture</Text>
+            </View>
+            
+          </TouchableOpacity>      
+              </View>
+              
+            </View>
+            
+      </Camera>
     </View>
   );
 }
@@ -13,8 +131,52 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    margin: 20,
+  },
+  button: {
+    flex: 0.1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    color: 'white',
+  },
+  maskOutter: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "space-around"
+  },
+  maskInner: {
+    width: 300,
+    backgroundColor: "transparent"    
+  },
+  maskFrame: {    
+    backgroundColor: "#000",
+    opacity: 0.7
+  },
+  maskRow: {
+    width: "100%"
+  },
+  maskCenter: { flexDirection: "row" },
+  rectangleText: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    flex: 1,
+    textAlign: "center",
+    color: "white"
+  }
 });
